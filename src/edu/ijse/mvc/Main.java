2568d4135d2013e712098781b34aacbe85900902
sakuni <saakfernandz@gmail.com>
2024-09-11 11:41:14 +0530

    config DBConnection

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -13,7 +13,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException,SQL{
         // TODO code application logic here
     }
     
